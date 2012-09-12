bdfa198a533fab7542b6fed696f96afbb64eb27e
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2012-09-12 23:40:53 +0200

    Update TODO. Add print statements to the operators files.

--- test/operators.lisp
+++ test/operators.lisp
@@ -1,22 +1,33 @@
 
+(print " > Common operators")
 (+ 4 3)
 (- 3 2)
 (* 2 3)
 (/ 10 2)
 
+(print "")
+(print " > Greater than")
 (> 3 2)
 (> 2 3)
 
+(print "")
+(print " > Less than")
 (< 2 3)
 (< 3 2)
 
+(print "")
+(print " > Greater than or equal to")
 (>= 3 2)
 (>= 3 3)
 (>= 2 3)
 
+(print "")
+(print " > Less than or equal to")
 (<= 2 3)
 (<= 3 3)
 (<= 3 2)
 
+(print "")
+(print " > Equal to")
 (= 3 3)
 (= 2 3)
