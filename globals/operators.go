8cf785e0082c5bf93e4208e9addcf0bb8692fed9
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2012-09-12 18:07:03 +0200

    Add a test for all kinds of operators. Generic arithmetic funcs.

--- globals/operators.go
+++ globals/operators.go
@@ -4,21 +4,49 @@ import (
 	"fmt"
 )
 
-func Plus(args ...interface{}) (interface{}, error) {
+type intFunc func(a, b int) int
+
+var (
+	intFuncs = map[string]intFunc{
+		"plus": func(a, b int) int { return a + b },
+		"minus": func(a, b int) int { return a - b },
+		"times": func(a, b int) int { return a * b },
+		"divide": func(a, b int) int { return a / b },
+	}
+)
+
+func op(name string, args []interface{}) (interface{}, error) {
 	// Check that two arguments are provided at least
 	if len(args) < 2 {
-		return 0, fmt.Errorf("at least two params are needed for the plus operator")
+		return 0, fmt.Errorf("at least two params are needed for the %s operator", name)
 	}
 
 	// Try to sum the integers
 	_, ok := args[0].(int)
 	if ok {
 		var ac int
+		f := intFuncs[name]
 		for _, arg := range args {
-			ac += arg.(int)
+			ac = f(ac, arg.(int))
 		}
 		return ac, nil
 	}
 
-	return 0, fmt.Errorf("num not recognized")
+	return 0, fmt.Errorf("%s operator can't handle this kind of numbers", name)
+}
+
+func Plus(args ...interface{}) (interface{}, error) {
+	return op("plus", args)
+}
+
+func Minus(args ...interface{}) (interface{}, error) {
+	return op("plus", args)
+}
+
+func Times(args ...interface{}) (interface{}, error) {
+	return op("times", args)
+}
+
+func Divide(args ...interface{}) (interface{}, error) {
+	return op("divide", args)
 }
