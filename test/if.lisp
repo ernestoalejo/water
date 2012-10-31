41e99b04634106300c6b3b7e4ff96084fea3c09e
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2012-10-31 09:49:33 +0100

    Update TODO. Add tests to check the return value of the if sentence.

--- test/if.lisp
+++ test/if.lisp
@@ -2,7 +2,12 @@
 (if (> 2 3) (println "mayor") (println "menor"))
 (if (> 4 3) (println "mayor") (println "menor"))
 
+(if (> 2 3) (+ 2 2) (+ 3 3))
+(if (> 4 3) (+ 2 2) (+ 3 3))
+
 ###########################################################
 
 menor
 mayor
+6
+4
